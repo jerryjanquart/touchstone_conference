f778ab3d1660ce0304e2d84eab228de13899e016
= <=>
2025-06-04 08:18:51 -0500

    fix spelling

--- resources/views/components/hotels.blade.php
+++ resources/views/components/hotels.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-lg-6 offset-lg-3">
                 <div class="text-center mb-xl-7 mb-5">
-                    <h2 class="text-black mb-3" style="font-family: 'Times New Roman', Times, serif; font-size: 2rem; font-weight: bold;">HOTEL ACCOMODATIONS</h2>
+                    <h2 class="text-black mb-3" style="font-family: 'Times New Roman', Times, serif; font-size: 2rem; font-weight: bold;">HOTEL ACCOMMODATIONS</h2>
                 </div>
             </div>
         </div>
