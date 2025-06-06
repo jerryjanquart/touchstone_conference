c16b6949a8df2e09070102d145268c2e74f6ecc2
= <=>
2025-06-06 10:27:57 -0500

    text fixes

--- resources/views/components/hotels.blade.php
+++ resources/views/components/hotels.blade.php
@@ -1,5 +1,5 @@
 <!--Customer stories start-->
-<section class="pb-xl-8 pb-5 pt-xl-5 pt-3 bg-my-gray2">
+<section class="pb-xl-8 pb-5 pt-xl-5 pt-3 bg-light">
     <div class="container">
         <div class="row">
             <div class="col-lg-6 offset-lg-3">
@@ -21,10 +21,10 @@
                 <img src="../assets/images/hotels/laquinta.jpg" class="card-img-top " alt="img">
                 <div class="card-body">
                     <h5 class="card-title text-black">La Quinta Inn &amp; Suites by Wyndham</h5>
-                    <p class="card-text">
-                        <em><span style="color: black;">*Walking distance from Trinity!</em><br />
+                    <p class="card-text text-black">
+                        <em>*Walking distance from Trinity!</em><br />
                         2000 Lakeside Dr.<br />
-                        Bannockburn, IL 60015</span>
+                        Bannockburn, IL 60015
                     </p>
                     <a href="https://www.wyndhamhotels.com/laquinta/bannockburn-illinois/la-quinta-bannockburn-deerfield/rooms-rates?brand_id=LQ&checkInDate=9/25/2025&checkOutDate=9/27/2025&useWRPoints=false&children=0&groupCode=stone2025&adults=1&rooms=1&loc=ChIJO0GKwZO_D4gRDxYCL73Z5i0&sessionId=1743696263" class="btn btn-dark">Book Now</a>
                 </div>
@@ -39,10 +39,10 @@
                 <img src="../assets/images/hotels/homewood.jpg" class="card-img-top " alt="img">
                 <div class="card-body">
                     <h5 class="card-title text-black">Homewood Suites by Hilton</h5>
-                    <p class="card-text">
-                        <em><span style="color: black;">*Only 2 miles from Trinity</em><br />
+                    <p class="card-text text-black">
+                        <em>*Only 2 miles from Trinity</em><br />
                         10 Westminster Way<br />
-                        Lincolnshire, IL 60069</span>
+                        Lincolnshire, IL 60069
                     </p>
                     <a href="https://www.hilton.com/en/book/reservation/deeplink/?ctyhocn=CHILSHW&amp;groupCode=CHW903&amp;arrivaldate=2025-09-24&amp;departuredate=2025-09-27&amp;cid=OM,WW,HILTONLINK,EN,DirectLink&amp;fromId=HILTONLINKDIRECT" class="btn btn-dark">Book Now</a>
                 </div>
