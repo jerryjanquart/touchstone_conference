ab0fd8e359860a73740f25fa101a386e70ea6dbf
= <=>
2025-06-06 11:09:47 -0500

    reapply color to hotels bg

--- resources/views/components/hotels.blade.php
+++ resources/views/components/hotels.blade.php
@@ -1,5 +1,5 @@
 <!--Customer stories start-->
-<section class="pb-xl-8 pb-5 pt-xl-5 pt-3 bg-light">
+<section class="pb-xl-8 pb-5 pt-xl-5 pt-3 bg-my-gray2">
     <div class="container">
         <div class="row">
             <div class="col-lg-6 offset-lg-3">
