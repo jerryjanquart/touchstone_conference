28d67ecf881dccadd3a01aeda05bd4e0a252567e
= <=>
2025-08-19 12:38:04 -0500

    add redirects for reduced rate

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/allsaints', function () {
+    return redirect()->away('https://wl.donorperfect.net/weblink/WebLink.aspx?name=E350987&id=98');
+});
+
+Route::get('/christthesavior', function () {
+    return redirect()->away('https://wl.donorperfect.net/weblink/WebLink.aspx?name=E350987&id=98');
+});
