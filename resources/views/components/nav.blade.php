790f9dea48dd8c2fc59114a2809c91ac37d119f3
= <=>
2025-12-08 13:01:52 -0600

    add videos to nav link

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -8,7 +8,7 @@
          </a>
          
          <div class="mt-3 mt-lg-0 d-flex justify-content-end">
-            <a style="text-decoration: none;" href="/livestream" class="btn btn-primary text-black"><u>All Videos</u></a> 
+            <a style="text-decoration: none;" href="https://www.touchstonemag.com/conferences/conference.php?year=2025" class="btn btn-primary text-black"><u>All Videos</u></a> 
             <a style="text-decoration: none;" href="/photos" class="btn btn-dark text-white"><u>Photo Gallery</u></a> 
          </div>
          
