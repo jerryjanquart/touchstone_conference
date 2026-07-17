f861e3e868db2a68a0f6b448f19dfb5eaadff375
Jerry Janquart <jmjanquart@gmail.com>
2026-07-17 10:58:18 -0500

    make photos in nav link directly to the photos page rather than the photos component on the homepage

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="pb-6 px-4">
     <div class="flex flex-wrap justify-center gap-x-6 gap-y-3 md:gap-x-10 font-serif text-base">
 
-        <a href="/#photos" class="text-stone-500 hover:text-black transition-colors duration-300">
+        <a href="/photos" class="text-stone-500 hover:text-black transition-colors duration-300">
             Photos
         </a>
 
