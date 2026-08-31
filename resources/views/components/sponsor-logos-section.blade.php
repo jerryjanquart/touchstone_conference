1e21912d7e2f45fcbc77118a64b37c7086539fb7
Jerry Janquart <jmjanquart@gmail.com>
2026-08-31 14:08:13 -0500

    lets add iconochki and eighth day to the homepage under sponsors

--- resources/views/components/sponsor-logos-section.blade.php
+++ resources/views/components/sponsor-logos-section.blade.php
@@ -12,6 +12,19 @@
         <div class="mt-6 mb-6 flex flex-col items-center gap-16">
 
             <a
+                href="https://www.eighthdaybooks.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="block w-[250px] max-w-full pt-7"
+            >
+                <img
+                    src="{{ asset('assets/images/conference/homepage/sponsors/eighth-day.jpg') }}"
+                    alt="Visit Eighth Day Books"
+                    class="h-auto w-full"
+                />
+            </a>
+
+            <a
                 href="https://creedandculture.com/"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -46,6 +59,19 @@
                 <img
                     src="{{ asset('assets/images/conference/homepage/sponsors/all-saints.jpg') }}"
                     alt="Visit All Saints"
+                    class="h-auto w-full"
+                />
+            </a>
+
+            <a
+                href="https://iconochki.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="block w-[300px] max-w-full"
+            >
+                <img
+                    src="{{ asset('assets/images/conference/homepage/sponsors/iconochki.jpg') }}"
+                    alt="Visit Iconochki"
                     class="h-auto w-full"
                 />
             </a>
